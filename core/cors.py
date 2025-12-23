6e951c31a3fef451b13e7468c5d1b45c49ba9b14
Prom1234pro <promiseimadonmwinyi@gmail.com>
2025-12-23 16:07:17 +0100

    removed origin

--- core/cors.py
+++ core/cors.py
@@ -4,7 +4,7 @@ from core.config import ALLOWED_ORIGIN
 def setup_cors(app):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=[ALLOWED_ORIGIN],
+        allow_origins=["*"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
